99a997bb7195e9ac58064b7698c890c140f81d62
Nguyen Dang Duc <dev.ndangduc.bn@gmail.com>
2021-07-24 17:15:08 +0700

    Add Code

---
+++ go.mod
@@ -0,0 +1,5 @@
+module golang_echo_framework
+
+go 1.16
+
+require github.com/labstack/echo/v4 v4.4.0
---
+++ server.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+/*
+	- Ngày: 24.07.2021
+	- By: Nguyễn Đăng Đức
+ */
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
